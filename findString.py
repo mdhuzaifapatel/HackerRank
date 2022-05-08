271958c279447db51395d51003e574f98b781923
Muhammad Huzaifa <2kl19ec045@klescet.ac.in>
2022-05-09 01:43:19 +0530

    Find String Problem
    https://www.hackerrank.com/challenges/find-a-string/problem

---
+++ findString.py
@@ -0,0 +1,17 @@
+# Programmed by Huzaifa
+
+def count_substring(string, sub_string):
+    count = 0
+    a = len(sub_string)
+    for i in range(len(string)):
+        if string[i:i+a] == sub_string:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
